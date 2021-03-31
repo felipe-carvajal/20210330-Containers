3e73408703f0fc3c02615437b09c6fd0bee628b9
Felipe Carvajal <fcarvajal@salesforce.com>
2021-03-31 11:54:55 -0400

    No need for this

--- force-app/main/default/lwc/test/test.js
+++
@@ -1,5 +0,0 @@
-import { LightningElement, wire } from 'lwc';
-import getAllAircraftp from '@salesforce/apex/AircraftController.getAllAircrafts';
-export default class Test extends LightningElement {
-    @wire (getAllAircraftp) aircrafts;
-}
