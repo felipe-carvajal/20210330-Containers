d2fba91192a9a34992f41f05ca5e241916fadc28
Felipe Carvajal <fcarvajal@salesforce.com>
2021-03-31 21:09:04 -0400

    Commit3

--- force-app/main/default/lwc/updatetile/updatetile.js
+++ force-app/main/default/lwc/updatetile/updatetile.js
@@ -2,6 +2,33 @@ import { LightningElement, track, wire, api } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 
 import getAllUsers from '@salesforce/apex/FindUsers.getAllUsers';
+const columns = [
+    {label: 'Opportunity name', fieldName: 'opportunityName', type: 'text'},
+    {label: 'Confidence', fieldName: 'confidence', type: 'percent', cellAttributes:
+    { iconName: { fieldName: 'trendIcon' }, iconPosition: 'right' }},
+    {label: 'Amount', fieldName: 'amount', type: 'currency', typeAttributes: { currencyCode: 'EUR', step: '0.001'}},
+    {label: 'Contact Email', fieldName: 'contact', type: 'email'},
+    {label: 'Contact Phone', fieldName: 'phone', type: 'phone'},
+];
+const data = [{
+    id: 'a',
+    opportunityName: 'Cloudhub',
+    confidence: 0.2,
+    amount: 25000,
+    contact: 'jrogers@cloudhub.com',
+    phone: '2352235235',
+    trendIcon: 'utility:down'
+},
+{
+    id: 'b',
+    opportunityName: 'Quip',
+    confidence: 0.78,
+    amount: 740000,
+    contact: 'quipy@quip.com',
+    phone: '2352235235',
+    trendIcon: 'utility:up'
+}];
+
 
 export default class ButtonIconStatefulBasic extends LightningElement {
     @track likeState = false;
@@ -15,7 +42,8 @@ export default class ButtonIconStatefulBasic extends LightningElement {
     // Copied structure
 
     @wire (getAllUsers) users;
-
+    data = data;
+    columns = columns;
 // Ends Here ---------
 
     handleLikeButtonClick() {
