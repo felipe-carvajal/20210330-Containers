450a3c1a11fd49447849b4c341c2a0059d1aefea
Felipe Carvajal <fcarvajal@salesforce.com>
2021-03-31 22:20:25 -0400

    commit5

--- force-app/main/default/lwc/updatetile/updatetile.js
+++ force-app/main/default/lwc/updatetile/updatetile.js
@@ -28,15 +28,23 @@ const data = [{
     phone: '2352235235',
     trendIcon: 'utility:up'
 }];
-const columnsfel = [
+/*const columnsfel = [
     {label: 'Name', fieldName: 'Name', type: 'text'},
     {label: 'Title', fieldName: 'Title', type: 'text', cellAttributes:
     { iconName: { fieldName: 'trendIcon' }, iconPosition: 'right' }},
     /*{label: 'Amount', fieldName: 'amount', type: 'currency', typeAttributes: { currencyCode: 'EUR', step: '0.001'}},
     {label: 'Contact Email', fieldName: 'contact', type: 'email'},
-    {label: 'Contact Phone', fieldName: 'phone', type: 'phone'},*/
+    {label: 'Contact Phone', fieldName: 'phone', type: 'phone'},
+];*/
+import IDEN_FIELD from '@salesforce/schema/User.Id';
+import NOMBRE_FIELD from '@salesforce/schema/User.Name';
+import TITLE_FIELD from '@salesforce/schema/User.Title';
+const COLUMNSFEL = [
+
+    { label: 'Employee Name', fieldName: NOMBRE_FIELD.fieldApiName, type: 'text' },
+    { label: 'Title', fieldName: TITLE_FIELD.fieldApiName, type: 'text' },
 ];
-export default class ButtonIconStatefulBasic extends LightningElement {
+    export default class ButtonIconStatefulBasic extends LightningElement {
     @track likeState = false;
     @track answerState = false;
     @track likeStateSize01 = false;
@@ -46,7 +54,7 @@ export default class ButtonIconStatefulBasic extends LightningElement {
     @track likeStateDisabled = false;
     @track answerStateDisabled = false;
     // Copied structure
-columnsfel=columnsfel;
+columnsfel=COLUMNSFEL;
     @wire (getAllUsers) users;
     data = data;
     columns = columns;
